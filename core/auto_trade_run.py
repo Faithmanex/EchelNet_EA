62cf6f4d9ea24ad8c3cdec41e1b046f0c236f291
Kemzzy-Dev <kemzzy17@gmail.com>
2024-02-18 19:01:52 +0100

    God abeg

--- core/auto_trade_run.py
+++ core/auto_trade_run.py
@@ -19,19 +19,22 @@ else:
 def get_calendar_data():
     script_path = os.path.join(os.path.dirname(__file__), 'forex_calendar.py') 
     # os.system(f'python {script_path}')
-    subprocess.run([python_path, script_path], check=True)
+    print(os.path.dirname(__file__))
+    subprocess.run([python_path, script_path], check=True, cwd=os.path.dirname(__file__))
 
 def run_auto_bot():
     script_path = os.path.join(os.path.dirname(__file__), 'Auto_Trade.py')
     os.system(f'python {script_path}')
 
-# Schedule the job to run daily at 10:00
-schedule.every().day.at("01:00").do(get_calendar_data)
+# Schedule the job to run  at 10:00
+schedule.every().day.at("22:56").do(get_calendar_data)
 
-schedule.every().day.at("01:02").do(run_auto_bot)
+# schedule.every().day.at("22:46").do(run_auto_bot)
 
 def run_automatically():
     print("auto bot triggered")
     while True:
         schedule.run_pending()
         time.sleep(20)
+
+run_automatically()
